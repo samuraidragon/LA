eb55ec8168f876af380f97146b5a092cdf040429
Katsumi Suenami <samudora42@gmail.com>
2016-12-23 07:55:39 +0900

    タイトル画面の導入

---
+++ Assets/startanim.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class startanim : MonoBehaviour {
+	public Animator anim;
+
+	float time;
+	bool countfklag;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		countfklag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			anim.SetBool ("start", true);
+		}
+
+		if (countfklag == true) {
+			time += Time.deltaTime;
+			Debug.Log (time);
+			if (time > 2) {
+				SceneManager.LoadScene ("defalme");
+			}
+		}
+
+	}
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Tree") {
+			countfklag = true;
+			}
+		}
+
+}
